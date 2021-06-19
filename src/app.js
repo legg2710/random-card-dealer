48803776475721cf93b6250112e9ba3d8eaa5c40
legg2710 <luisemilio2710@gmail.com>
2021-06-19 21:22:23 +0000

    random card preview

--- src/app.js
+++ src/app.js
@@ -4,22 +4,22 @@ import "./style.css";
 
 window.onload = () => {
   //Variables
-  let pint = ["♦", "♥", "♠", "♣"];
+  let pintas = ["♦", "♥", "♠", "♣"];
   let num = ["A", "J", "Q", "K", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
   //Function Randomizer
   function randomizer() {
-    let ranpint = Math.floor(Math.random() * pint.length);
-    return ranpint;
+    let ranpint = Math.floor(Math.random() * pintas.length);
+    return pintas[ranpint];
   }
 
-  function randomizer() {
+  function randomizer2() {
     let rannum = Math.floor(Math.random() * num.length);
     return rannum;
   }
 
   //Function link
-  document.getElementById("number-left").innerHTML = randomizer(num);
-  document.getElementById("pinta").innerHTML = randomizer(pint);
-  document.getElementById("number-right").innerHTML = randomizer(pint);
+  document.getElementById("number-left").innerHTML = randomizer2();
+  document.getElementById("pinta").innerHTML = randomizer();
+  document.getElementById("number-right").innerHTML = randomizer2();
 };
